bca1876de9b80960eec6d4d66529d211f4b1cb5a
razitazi <razi@razitazi.com>
2017-12-14 14:40:07 +0100

    Added anime.js. Tested and working

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -7,11 +7,15 @@
  * Copyright 2017. MIT licensed.
  */
 (function ($, window, document, undefined) {
-
+  
   'use strict';
-
+  
   $(function () {
-    
+    // var cssSelector = anime({
+    //   targets: '#cssSelector .el',
+    //   translateX: 250,
+    //   autostart: true
+    // });
   });
-
+  
 })(jQuery, window, document);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,13 @@
 (function ($, window, document, undefined) {
-
+  
   'use strict';
-
+  
   $(function () {
-    
+    // var cssSelector = anime({
+    //   targets: '#cssSelector .el',
+    //   translateX: 250,
+    //   autostart: true
+    // });
   });
-
+  
 })(jQuery, window, document);
