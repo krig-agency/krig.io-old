21b6f1d4a9152e46593feb739b1f9711946fb3af
Johannes <johannes@jitesoft.com>
2017-12-16 11:01:28 +0100

    Updated gulp and gulp-sass plugins to latest versions (gulp4-beta). Updated gulpscript to use latest gulp and to run tasks in series or parallel as intended.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    header  = require('gulp-header'),
-    rename = require('gulp-rename'),
-    cssnano = require('gulp-cssnano'),
-    sourcemaps = require('gulp-sourcemaps'),
-    package = require('./package.json'),
-    eslint = require('gulp-eslint'),
-    webpack = require('webpack-stream'),
-    gulpSequence = require('gulp-sequence');
-
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const header = require('gulp-header');
+const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const pkg = require('./package.json');
+const eslint = require('gulp-eslint');
+const webpack = require('webpack-stream');
 
 const banner = [
   '/*!\n' +
@@ -25,28 +23,33 @@ const banner = [
   '\n'
 ].join('');
 
-gulp.task('css', function () {
-    return gulp.src('src/scss/style.scss')
+let cssCompile = () => {
+  return gulp.src('src/scss/style.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass())
     .pipe(autoprefixer('last 4 version'))
     .pipe(gulp.dest('app/assets/css'))
     .pipe(cssnano())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(header(banner, { package : package }))
+    .pipe(header(banner, { package: pkg }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('app/assets/css'))
-    .pipe(browserSync.reload({stream:true}));
-});
+    .pipe(browserSync.reload({stream: true}));
+};
+cssCompile.displayName = 'compile-css';
+cssCompile.description = 'Builds and moves the SCSS files in the src/scss directory to the app/assets/css directory.';
+gulp.task(cssCompile);
 
-gulp.task('lint-js', function() {
-
+let jsLint = () => {
   return gulp.src('src/js/**/*.js')
     .pipe(eslint())
-    .pipe(eslint.format())
-});
+    .pipe(eslint.format());
+};
+jsLint.displayName = 'lint-js';
+jsLint.description = 'Runs ESLint with semistandard code standard.';
+gulp.task(jsLint);
 
-gulp.task('compile-js', function() {
+let jsCompile = () => {
   return gulp.src('src/js/**/*.js')
     .pipe(webpack(
       {
@@ -55,7 +58,7 @@ gulp.task('compile-js', function() {
           // If more than one entrypoint exists, add it here and a new file will be created on build.
         },
         output: {
-          filename: '[name].js',
+          filename: '[name].js'
         },
         module: {
           loaders: [{
@@ -69,50 +72,73 @@ gulp.task('compile-js', function() {
       }
     ))
     .pipe(sourcemaps.init())
-    .pipe(header(banner, { package : package }))
+    .pipe(header(banner, { package: pkg }))
     .pipe(gulp.dest('app/assets/js'))
     .pipe(uglify())
-    .pipe(header(banner, { package : package }))
+    .pipe(header(banner, { package: pkg }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('app/assets/js'));
-});
+};
+jsCompile.displayName = 'compile-js';
+jsCompile.description = 'Builds the JavaScript sourcecode with Webpack.';
+gulp.task(jsCompile);
 
-gulp.task('js', (cb) => { gulpSequence('compile-js', 'lint-js')(cb); });
+let sync = () => {
+  return browserSync.init(null, {
+    server: {
+      baseDir: 'app'
+    }
+  });
+};
+sync.displayName = 'browser-sync';
+sync.description = 'Initializes browser sync.';
+gulp.task(sync);
 
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "app"
-        }
-    });
-});
+let reSync = () => {
+  return browserSync.reload();
+};
+reSync.displayName = 'browser-reload';
+reSync.description = 'Reloads the page that browser sync serves.';
+gulp.task(reSync);
 
-gulp.task('bs-reload', function () {
-    browserSync.reload();
-});
-
-gulp.task('default', ['watch']);
-
-gulp.task('html', function() {
-  gulp.src('src/html/**/*.html')
+let moveHtml = () => {
+  return gulp.src('src/html/**/*.html')
     .pipe(gulp.dest('app'));
-});
+};
+moveHtml.displayName = 'move-html';
+moveHtml.description = 'Moves all static html files from /src to /app.';
+gulp.task(moveHtml);
 
-gulp.task('images', function() {
-  gulp.src('src/img/**/*')
-    .pipe(gulp.dest('app/assets/img'));
-});
+let moveImages = gulp.parallel(
+  function moveImg () {
+    return gulp.src('src/img/**/*')
+      .pipe(gulp.dest('app/assets/img'));
+  },
+  function moveFavicon () {
+    return gulp.src('src/favicon/**/*')
+      .pipe(gulp.dest('app/'));
+  }
+);
+moveImages.displayName = 'move-images';
+moveImages.description = 'Moves images from the /src/img & /src/favicon directories to the /app directory.';
+gulp.task(moveImages);
 
-gulp.task('icons', function() {
-  gulp.src('src/favicon/**/*')
-    .pipe(gulp.dest('app/'));
-});
+let watchTask = function watch () {
+  gulp.watch('src/js/**/*.js').on('all', gulp.series('compile-js', 'lint-js', 'browser-reload'));
+  gulp.watch('src/scss/**/*.scss').on('all', gulp.series('compile-css', 'browser-reload'));
+  gulp.watch('src/html/**/*').on('all', gulp.series('move-html', 'browser-reload'));
+  gulp.watch(['src/favicon/**/*', 'src/img/**/*']).on('all', gulp.series('move-images', 'browser-reload'));
+};
+watchTask.displayName = 'watch';
+watchTask.description = 'Watches files for changes and invokes its given move or compile scripts..';
+gulp.task(watchTask);
 
-gulp.task('watch', gulpSequence(['css', 'js', 'icons', 'images', 'html'], 'browser-sync', function(cb) {
-  gulp.watch('src/scss/**/*.scss', ['css', 'bs-reload']);
-  gulp.watch('src/js/**/*.js', ['js', 'bs-reload']);
-  gulp.watch('src/html/**/*', ['html', 'bs-reload']);
-  gulp.watch('src/favicon/**/*', ['icons', 'bs-reload']);
-  gulp.watch('src/images/**/*', ['images', 'bs-reload']);
-}));
+let defaultTask = gulp.series(
+  gulp.parallel('compile-css', 'compile-js', 'move-html', 'move-images'),
+  'lint-js',
+  gulp.parallel('browser-sync', 'watch')
+);
+defaultTask.displayName = 'default';
+defaultTask.description = 'Default task. Rebuilds the application and starts the watcher.';
+gulp.task(defaultTask);
