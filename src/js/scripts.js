fd447615ffb44a5566d7da0e39c28c4dbaca29e0
razitazi <razi@razitazi.com>
2018-01-14 12:05:16 +0100

    animejs test

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,22 @@
 import modernizer from '../components/modernizr'; // eslint-disable-line
-import jQuery from 'jquery';
-import menu from './menu.js'; // eslint-disable-line
+import anime from '../components/anime.min.js'; // eslint-disable-line
 
-(function ($, window, document) {
+import jQuery from 'jquery';
+
+(function ($) {
   'use strict';
-  //   translateX: 250,
-  //   autostart: true
-  // var cssSelector = anime({
-  //   targets: '#cssSelector .el',
-  // });
+  var bouncingBall = anime({
+    targets: '.work-panel__bar',
+    translateY: '50vh',
+    duration: 300,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInCubic',
+    scaleX: {
+      value: 1.05,
+      duration: 150,
+      delay: 268
+    }
+  });
 });
-(jQuery, window, document);
+(jQuery);
