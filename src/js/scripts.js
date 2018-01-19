58c1381415441906ae23128a03e4466d5c69f9da
Johannes <johannes@jitesoft.com>
2018-01-19 14:08:57 +0100

    Added jquery-scrollify dependency and loaded it in scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 import menu from '../components/menu'; // eslint-disable-line
 import Background from './Background';
 import $ from 'jquery';
+require('jquery-scrollify');
 
 document.addEventListener('DOMContentLoaded', function () {
   let bg = new Background(document.getElementById('nokey'));
