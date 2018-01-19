0d3987e782893145dc09691b139b2ebc2e3f986f
Johannes <johannes@jitesoft.com>
2018-01-19 14:05:46 +0100

    Added jquery.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 import menu from '../components/menu'; // eslint-disable-line
 import Background from './Background';
+import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function () {
   let bg = new Background(document.getElementById('nokey'));
