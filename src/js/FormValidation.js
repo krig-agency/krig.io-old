cc5808999d2c6ae91253d689e3b361e681c33b94
Johannes <johannes@jitesoft.com>
2018-02-28 11:53:08 +0100

    Finalized form validation class and set up event handlers in the scripts.js file.

--- src/js/FormValidation.js
+++ src/js/FormValidation.js
@@ -6,23 +6,40 @@ export default class FormValidation extends EventEmitter {
 
     this.form = form;
     this.rules = rules;
-
     this.validationTypes = {
       'email': (element) => {
-        let email = element.value();
-        return '/\\S+@\\S+/'.test(email) ? true : 'Not a valid email address.';
+        return element.value.indexOf('@') > 0 ? true : 'Not a valid email address.';
       },
       'not-empty': (element) => {
-        return (element.value().length > 0 || element.innerText.length > 0) ? true : 'Input was empty.';
+        return (element.value.length > 0 || element.innerText.length > 0) ? true : 'Input was empty.';
       }
     };
+
+    this.form.addEventListener('submit', this.submit.bind(this));
+    Object.entries(this.rules).forEach((entry) => {
+      this.form.querySelector(entry[0]).addEventListener('input', this.change.bind(this));
+    });
   }
 
-  submit () {
-    this.form.preventDefault();
+  /**
+   * Method handling input change events on each form input with a rule.
+   * @param event
+   */
+  change (event) {
+    this.emit('change', event.target);
+  }
+
+  /**
+   * Method handling submit event to force validation and pause submit.
+   * @param event
+   */
+  submit (event) {
+    event.preventDefault();
 
     let validationErrors = [];
-    this.rules.forEach((rules, selector) => {
+    Object.entries(this.rules).forEach((rule) => {
+      let selector = rule[0];
+      let rules = rule[1];
       let element = this.form.querySelector(selector);
       for (let test of rules) {
         let output = this.validationTypes[test](element);
@@ -39,7 +56,9 @@ export default class FormValidation extends EventEmitter {
     });
 
     if (validationErrors.length === 0) {
-      this.emit('success', (this.form));
+      if (this.emit('success', (this.form))) {
+        this.form.submit();
+      }
     } else {
       this.emit('failure', validationErrors);
     }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,7 @@ import CookieConsent from './CookieConsent';
 import 'jquery-scrollify';
 import 'babel-polyfill';
 import 'babel-core/register';
+import FormValidation from './FormValidation';
 
 $(function () {
   $.scrollify({
@@ -25,5 +26,26 @@ $(function () {
     script.async = true;
     script.src = './assets/js/postcookie.js';
     document.querySelector('body').appendChild(script);
+  });
+
+  let formValidation = new FormValidation(
+    document.querySelector('form.pitch-form__form'),
+    {
+      'input[name="Description"]': [ 'not-empty' ],
+      'input[name="Budget"]': [ 'not-empty' ],
+      'input[name="Email"]': [ 'not-empty', 'email' ]
+    }
+  );
+
+  formValidation.on('change', (e) => {
+    e.classList.remove('error');
+  });
+
+  formValidation.on('success', () => true);
+
+  formValidation.on('failure', (validationErrors) => {
+    validationErrors.forEach((error) => {
+      error.element.classList.add('error');
+    });
   });
 });
