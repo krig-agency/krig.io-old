3547e6bc1c5697115ce768fc84df5232ee22bf07
Nadal Kristall <lucas@krig.io>
2018-06-21 18:29:34 +0200

    Fixed form validation UX

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -28,41 +28,50 @@ $(function () {
     acceptButton: '.cookies__accept'
   });
 
-  cookieConsent.active().then((accepted) => {
-    if (accepted) {
-      let script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.src = './assets/js/postcookie.js';
-      document.querySelector('body').appendChild(script);
-    }
-  }).catch((error) => {
-    if (window.console) {
-      console.error(error);
-    }
-    document.querySelector('div.cookies').classList.add('hidden');
-  });
+  cookieConsent
+    .active()
+    .then(accepted => {
+      if (accepted) {
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = './assets/js/postcookie.js';
+        document.querySelector('body').appendChild(script);
+      }
+    })
+    .catch(error => {
+      if (window.console) {
+        console.error(error);
+      }
+      document.querySelector('div.cookies').classList.add('hidden');
+    });
 
   let formValidation = new FormValidation(
     document.querySelector('form.pitch-form__form'),
     {
-      'textarea[name="Description"]': [ 'not-empty' ],
-      'input[name="Budget"]': [ 'not-empty' ],
-      'input[name="Email"]': [ 'not-empty', 'email' ]
+      'textarea[name="Description"]': ['not-empty'],
+      'input[name="Budget"]': ['not-empty'],
+      'input[name="Email"]': ['not-empty', 'email']
     }
   );
 
-  formValidation.on('change', (e) => {
+  formValidation.on('change', e => {
     e.classList.remove('error');
   });
 
-  formValidation.on('success', (form) => {
+  formValidation.on('success', form => {
     const description = document.getElementsByName('Description')[0];
     const budget = document.getElementsByName('Budget')[0];
     const email = document.getElementsByName('Email')[0];
     const params = {
       to: 'webmaster@krig.io',
-      body: 'Beskrivning:\n' + description.value + '\n\nBudget:\n' + budget.value + '\n\nE-post:\n' + email.value,
+      body:
+        'Beskrivning:\n' +
+        description.value +
+        '\n\nBudget:\n' +
+        budget.value +
+        '\n\nE-post:\n' +
+        email.value,
       subject: 'Pitch'
     };
 
@@ -71,25 +80,39 @@ $(function () {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
       },
-      body: Object.keys(params).map((key) => {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-      }).join('&')
-    }).then(res => {
-      description.value = '';
-      budget.value = '';
-      email.value = '';
-      menu.classList.toggle('open');
-    }).catch(err => {
-      if (window.console) {
-        console.log(err);
-      }
-    });
+      body: Object.keys(params)
+        .map(key => {
+          return (
+            encodeURIComponent(key) + '=' + encodeURIComponent(params[key])
+          );
+        })
+        .join('&')
+    })
+      .then(res => {
+        description.value = '';
+        budget.value = '';
+        email.value = '';
+        menu.classList.toggle('open');
+      })
+      .catch(err => {
+        if (window.console) {
+          console.log(err);
+        }
+      });
   });
 
-  formValidation.on('failure', (validationErrors) => {
-    validationErrors.forEach((error) => {
+  formValidation.on('failure', validationErrors => {
+    let button = document.querySelector('.btn--form');
+
+    validationErrors.forEach(error => {
       error.element.classList.add('error');
     });
+
+    button.classList.add('shake');
+
+    setTimeout(() => {
+      button.classList.remove('shake');
+    }, 1000);
   });
 
   // Load texts.
